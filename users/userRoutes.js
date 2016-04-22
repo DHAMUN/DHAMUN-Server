e0991324dcb1f35413c514c730e548c94cef4bbe
Omar Shaikh <oshaikh13@gmail.com>
2016-04-22 23:52:59 +0300

    Add basic server with user account handling

---
+++ users/userRoutes.js
@@ -0,0 +1,13 @@
+var userController = require('./userController.js'),
+             helpers = require('../config/helpers.js');
+
+
+
+module.exports = function (app) {
+  // app === userRouter injected from middlware.js
+
+  app.post('/signin', userController.signin);
+  app.post('/signup', userController.signup);
+  app.get('/signedin', userController.checkAuth);
+
+};
