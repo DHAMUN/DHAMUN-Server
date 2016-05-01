352332a0fcaf36e4567c6d5e4dc0440c59efa67a
Omar Shaikh <oshaikh13@gmail.com>
2016-05-01 16:52:14 +0300

    (refactor) simplify comparing since auth procedure is different

--- users/userModel.js
+++ users/userModel.js
@@ -39,46 +39,9 @@ var UserSchema = new mongoose.Schema({
 
 });
 
-UserSchema.methods.comparePasswords = function (candidatePassword) {
-  var defer = Q.defer();
-  var savedPassword = this.password;
-  bcrypt.compare(candidatePassword, savedPassword, function (err, isMatch) {
-    if (err) {
-      defer.reject(err);
-    } else {
-      defer.resolve(isMatch);
-    }
-  });
-  return defer.promise;
+UserSchema.methods.compareCodes = function (candidateCode) {
+  var savedPassword = this.hashCode;
+  return this.hashCode === candidateCode;
 };
-
-UserSchema.pre('save', function (next) {
-  var user = this;
-
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) {
-    return next();
-  }
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) {
-      return next(err);
-    }
-
-    // hash the password along with our new salt
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) {
-        return next(err);
-      }
-
-      // Generate a unique URL.
-
-      user.password = hash;
-      user.salt = salt;
-      next();
-    });
-  });
-});
 
 module.exports = mongoose.model('users', UserSchema);
