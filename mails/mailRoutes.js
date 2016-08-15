490895815e37cd4f20c76a323d72184c23cadf87
Omar Shaikh <oshaikh13@gmail.com>
2016-08-14 23:09:06 -0400

    Dispatch mail from server

--- mails/mailRoutes.js
+++ mails/mailRoutes.js
@@ -6,6 +6,6 @@ var mailController = require('./mailController.js'),
 module.exports = function (app) {
   // app === userRouter injected from middlware.js
 
-  app.post('/send', mailController.send);
+  app.post('/send', helpers.decode, mailController.send);
 
 };
