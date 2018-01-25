a25da0c86bd75ff135342c263bd42e3eeedfad0e
Omar Shaikh <oshaikh13@gmail.com>
2018-01-25 08:19:40 +0300

    route handlers

---
+++ sockets/attendance.js
@@ -0,0 +1,32 @@
+
+var attendanceData = require('../committees/attendanceData.js');
+var jwt  = require('jwt-simple');
+
+function selectivelyStripID(attendanceData, user) {
+  return (user.userLevel !== "Delegate" ? attendanceData[user.committee] : 
+    Object.assign({}, attendanceData[user.committee], {verificationID: null}));
+}
+
+module.exports = {
+
+  markPresent: function(data){
+    var user = jwt.decode(data.token, process.env.TOKEN_SECRET);
+
+    if (user) {
+      attendanceData[user.committee][user.country] = true;
+      this.sockets.in(user.committee).emit("attendance update", selectivelyStripID(attendanceData, user));
+      console.log(user.firstName + " is marking himself present");
+    }
+
+  },
+
+  get: function(data){
+
+    var user = jwt.decode(data.token, process.env.TOKEN_SECRET);
+    if (user) {
+      this.emit("attendance update", selectivelyStripID(attendanceData, user));
+      console.log(user.firstName + " is getting attendance for " + user.committee);
+    }
+
+  },
+}
